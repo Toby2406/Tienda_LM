bb48ddb5f05a8f5ccb2369dd4685ef64c74a0a3b
Toby2406 <87052871+Toby2406@users.noreply.github.com>
2021-07-09 15:52:40 -0300

    Update conexion.php

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,10 +1,10 @@
 <?php
  class conectar{
    //Declaramos variables
-  private  $servidor = "localhost";
-  private  $usuario = "root";
-  private  $clave = "";
-  private  $bd = "tinda";
+  private  $servidor = "ec2-52-202-152-4.compute-1.amazonaws.com";
+  private  $usuario = "ocnnyoxnneovhs";
+  private  $clave = "de6e47d7fde3f613c12e0897b9f411ffba239781299d7a11aa38cb203a54f8e9";
+  private  $bd = "dc7j7k6ldeem3s";
   //Realizamos la coneion
 public function RealaizarConexion(){
     $conexion = mysqli_connect($this->servidor,$this->usuario,$this->clave,$this->bd);
