eac539e69d52613907824e9baa3adf712b4e0cd9
Toby2406 <87052871+Toby2406@users.noreply.github.com>
2021-07-11 00:54:33 -0300

    Update conexion.php

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -9,7 +9,7 @@
   //Realizamos la coneion
 public function RealaizarConexion(){
   try {
-    $conexion = new PDO_PGSQL('pgsql:host=$servidor;port=$port;dbname=$bd', $usuario, $clave);
+    $conexion = new PDO_PGSQL('pgsql:host=$servidor;port=$port;dbname=$bd;user=$usuario;password=$clave');
     foreach($conexion->query('SELECT * from FOO') as $fila) {
         print_r($fila);
     }
