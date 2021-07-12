7b3e211951096d64ec17f84a14122b10277ceff7
Toby2406 <87052871+Toby2406@users.noreply.github.com>
2021-07-12 19:25:23 -0300

    Update conexion.php

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -10,7 +10,7 @@
   //Realizamos la coneion
 public function RealaizarConexion()
 {
-    $conexion = pg_connect($this->cadena); 
+    $conexion = pg_connect("host='ec2-52-202-152-4.compute-1.amazonaws.com' port='5432' dbname='dc7j7k6ldeem3s'  user='ocnnyoxnneovhs' password='de6e47d7fde3f613c12e0897b9f411ffba239781299d7a11aa38cb203a54f8e9'"); 
    echo "adasda";
    if (!$conexion) 
     {
