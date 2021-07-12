ecde0d9a37190971d17f6a818ca55f69a947fee0
Toby2406 <87052871+Toby2406@users.noreply.github.com>
2021-07-12 13:10:38 -0300

    Update conexion.php

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -5,11 +5,11 @@
   private  $usuario = 'ocnnyoxnneovhs';
   private  $clave = 'de6e47d7fde3f613c12e0897b9f411ffba239781299d7a11aa38cb203a54f8e9';
   private  $bd = 'dc7j7k6ldeem3s';
-  private $port = 5432;
+  private $port = '5432';
   //Realizamos la coneion
 public function RealaizarConexion()
  {
-    $conexion = pg_connect("host=$servidor port=$port dbname=$bd user=$usuario password=$clave");
+    $conexion = pg_connect("host='ec2-52-202-152-4.compute-1.amazonaws.com' port=5432 dbname='dc7j7k6ldeem3s' user='ocnnyoxnneovhs' password='de6e47d7fde3f613c12e0897b9f411ffba239781299d7a11aa38cb203a54f8e9'");
     if(!$conexion)
     {
      echo "error en la conexion";
