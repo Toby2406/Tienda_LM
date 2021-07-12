5cc4e01965b1907b7d3c5a3e29e3014bb30d41e9
Toby2406 <87052871+Toby2406@users.noreply.github.com>
2021-07-12 13:22:42 -0300

    Update conexion.php

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -9,7 +9,7 @@
   //Realizamos la coneion
 public function RealaizarConexion()
 {
-    $conexion = pg_connect("host= $this->servidor port=$this->port dbname=$this->bd user=$this->usuario password=$this->clave");
+    $conexion = pg_connect("host='$this->servidor' port='$this->port' dbname='$this->bd' user='$this->usuario' password='$this->clave'");
     if (!$conexion) 
     {
       echo "<h4>Error en la conexion</h4>";
