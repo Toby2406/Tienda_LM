f241224ac6a50b77e2bc447b096112e0aa23cd9f
Toby2406 <87052871+Toby2406@users.noreply.github.com>
2021-07-12 12:43:58 -0300

    Update conexion.php

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,11 +1,11 @@
 <?php
  class conectar{
    //Declaramos variables
-  private  $servidor = "ec2-52-202-152-4.compute-1.amazonaws.com";
-  private  $usuario = "ocnnyoxnneovhs";
-  private  $clave = "de6e47d7fde3f613c12e0897b9f411ffba239781299d7a11aa38cb203a54f8e9";
-  private  $bd = "dc7j7k6ldeem3s";
-  private $port = "5432";
+  private  $servidor = 'ec2-52-202-152-4.compute-1.amazonaws.com';
+  private  $usuario = 'ocnnyoxnneovhs';
+  private  $clave = 'de6e47d7fde3f613c12e0897b9f411ffba239781299d7a11aa38cb203a54f8e9';
+  private  $bd = 'dc7j7k6ldeem3s';
+  private $port = '5432';
   //Realizamos la coneion
 public function RealaizarConexion()
  {
