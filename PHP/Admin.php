83cf4725988253998ee45be6b42321e7769d666b
Toby2406 <87052871+Toby2406@users.noreply.github.com>
2021-07-12 21:51:01 -0300

    Update Admin.php

--- PHP/Admin.php
+++ PHP/Admin.php
@@ -11,7 +11,7 @@ class Select
       //llamada a el metodo de realizar $conexion
       $R_conexion = $conec->RealaizarConexion();
       $consulta = "SELECT * FROM localidad order by codigopostal asc";
-      $ejecucion = mysqli_query($R_conexion, $consulta) or die (mysqli_error($R_conexion));
+      $ejecucion = pg_query($R_conexion, $consulta) or die (pg_error($R_conexion));
       foreach ($ejecucion as $valores) {
         echo "<option value = '$valores[codigopostal]'>",$valores['codigopostal'],"</option>";
       }
