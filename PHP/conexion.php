6dd868d14513c91759083a47e9b27dd1a7bb418f
Toby2406 <87052871+Toby2406@users.noreply.github.com>
2021-07-12 21:33:08 -0300

    Update conexion.php

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -6,11 +6,11 @@
   private  $clave = 'de6e47d7fde3f613c12e0897b9f411ffba239781299d7a11aa38cb203a54f8e9';
   private  $bd = 'dc7j7k6ldeem3s';
   private $port = '5432';
-  private $cadena = "host='$servidor' port='$port' dbname='$bd'  user='$usuario' password='$clave'";
+  private $cadena = "host='$servidor' dbname='$bd'  user='$usuario' password='$clave'";
   //Realizamos la coneion
 public function RealaizarConexion()
 {
-   $conexion = pg_connect("host='ec2-52-202-152-4.compute-1.amazonaws.com' port='5432' dbname='dc7j7k6ldeem3s'  user='ocnnyoxnneovhs' password='de6e47d7fde3f613c12e0897b9f411ffba239781299d7a11aa38cb203a54f8e9'"); 
+   $conexion = pg_connect($this->cadena); 
   
    if (!$conexion) 
     {
